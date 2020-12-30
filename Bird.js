35d211f459f4b3de0cb984a3b2b9fec64e2397fb
prabha-18 <prabhapremathavasi@gmail.com>
2020-12-30 12:02:28 +0530

    changing colour of the bird

--- Bird.js
+++ Bird.js
@@ -20,8 +20,8 @@ class Bird {
       translate(pos.x, pos.y);
       rotate(angle);
       strokeWeight(3);
-      stroke('blue')
-      fill('red')
+      stroke('black');
+      fill('yellow');
       rectMode(CENTER)
       rect(0, 0, this.width, this.height);
       pop();
